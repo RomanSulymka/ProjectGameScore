4ca97e6c79c10b47e53fc647c7cec4e29e503c0f
Roman Sulymka <roma.sulimak@gmail.com>
2021-01-15 12:15:34 +0200

    added 'enter 2 teams screen'

---
+++ app/src/main/java/com/example/projectgamescore/ListOfWinnersActivity.kt
@@ -0,0 +1,11 @@
+package com.example.projectgamescore
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ListOfWinnersActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_list_of_winners)
+    }
+}
